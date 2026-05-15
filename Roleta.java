4719d5cd6ee3e23b16c9afab9edae3cf1ea797aa
kauanss1 <kauansantossilva138@gmail.com>
2026-05-15 17:30:48 -0300

    controle de sorteio

--- Roleta.java
+++ Roleta.java
@@ -1,34 +1,36 @@
-
+import java.util.Scanner;
 public class Roleta {
     public static void main(String[] args) {
         
-        String [] duelista= {"jett", "reyna", "yoru", "raze", "phoenix", "neon", "iso"};
-        String[] controladores = {"omen", "brimstone", "viper", "astra", "harbor", "clove"};
-        String[] iniciadores = {"sova", "breach", "skye", "kayo", "fade", "gekko", "tejo"};
-        String[] sentinelas = {"sage", "cypher", "killjoy", "chamber", "deadlock", "vyse"};
-
-
-        Sorteio sorteio = new Sorteio(duelista); 
-        System.err.println(sorteio.sortear());
-
-        sorteio.setTagentes(controladores);
-        System.err.println(sorteio.sortear());
-
-        sorteio.setTagentes(iniciadores);
-        System.err.println(sorteio.sortear());
-        
-        sorteio.setTagentes(sentinelas);
-        System.err.println(sorteio.sortear());
-
-
+        String[] duelista = {"JETT", "REYNA", "YORU", "RAZE", "PHOENIX", "NEON", "ISO"};
+        String[] controladores = {"OMEN", "BRIMSTONE", "VIPER", "ASTRA", "HARBOR", "CLOVE"};
+        String[] iniciadores = {"SOVA", "BREACH", "SKYE", "KAYO", "FADE", "GEKKO", "TEJO"};
+        String[] sentinelas = {"SAGE", "CYPHER", "KILLJOY", "CHAMBER", "DEADLOCK", "VYSE"};
         String [][] cclass={ duelista,controladores,iniciadores,sentinelas };
+        int n = 1;
+        Scanner scanf = new Scanner(System.in);
 
+        do {
+            
+            Sorteio sorteio = new Sorteio(duelista); 
+            System.err.println(sorteio.sortear());
+            
+            sorteio.setTagentes(controladores);
+            System.err.println(sorteio.sortear());
+            
+            sorteio.setTagentes(iniciadores);
+            System.err.println(sorteio.sortear());
+            
+            sorteio.setTagentes(sentinelas);
+            System.err.println(sorteio.sortear());
+            
+            
+            sorteio.setTagentes(sorteio.sclass(cclass));
+            System.err.println(sorteio.sortear());
+            System.out.println("digite 0 para sair ou 1 para sortear novamente ");
+            n = scanf.nextInt();
 
-
-        sorteio.setTagentes(sorteio.sclass(cclass));
-
-
-        System.err.println(sorteio.sortear());
+        } while (n !=0);
         
         
         
